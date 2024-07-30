f319abcb85ff368fb90da9572799fa4506b01c18
gabriel_gc <108274965+gabriel123432@users.noreply.github.com>
2024-07-30 09:08:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,129 @@
-const caixaPrincipal = document.querySelector('.caixa-principal');
-const caixaPerguntas = document.querySelector('.caixa-perguntas');
-const caixaAlternativas = document.querySelector('.caixa-alternativas');
-const caixaResultado = document.querySelector('.caixa-resultado');
-const textoResultado = document.querySelector('.texto-resultado');
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
 const perguntas = [
     {
         enunciado: "Qual país tem o maior índice de pobreza?",
-        alternativas: ["África", "Somália", "Suíça", "Cuba"],
+        alternativas: [
+            {
+                texto:"África",
+                afirmação:"afirmação da alternativa 1°"
+            },
+            {
+                texto: "Somália",
+                afirmação: "afirmação alternativa 2°"
+            }
+            {
+                texto:"Suiça",
+                afirmação:"afirmação alternativa 3°"
+            },
+            {
+                texto: "Cuba",
+                afirmação: "afirmação alternativa 4°"
+            }
+        ]
     },
     {
         enunciado: "Qual o estado brasileiro mais racista?",
-        alternativas: ["São Paulo","Paraná","Rio Grande do Sul","Santa Catarina"],
+        alternativas: [
+            {
+                texto: "São Paulo",
+                afirmação: "afirmação alternativa 1°"
+            },
+            {
+                texto: "Paraná",
+                afirmação: "afirmação alternativa 2°"
+            },
+            {
+            texto:"Rio Grande do Sul",
+            afirmação:"afirmação alternativa 3°"
+        },
+        {
+            texto: "Santa Catarina",
+            afirmação: "afirmação alternativa 4°"
+        }
+        ]
     },
     {
         enunciado: "Qual dos fatores é mais frequntemente citado como uma causa primária da desiguldade social em sociedades modernas?",
-        alternativas: ["Distribuição desigual de riqueza e renda", "Localização geográfica", "Preferências culturais", "Condições climáticas"],
+        alternativas: [
+            {
+                texto: "Distribuição desigual de riqueza e renda",
+                afirmação: "afirmação alternativa 1°"
+            },
+            {
+                texto: "Localização geográfica",
+                afirmacao: "afirmação alternativa 2°"
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+            }
+        ]
     },
 ];
 
+
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
-function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
-    const botaoAlternativas = 
-    document.createElement ("button");
-    botaoAlternativas.textContent = alternativa;
-    caixaAlternativas.appendChild(botaoAlternativas);
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
 mostraPergunta();
